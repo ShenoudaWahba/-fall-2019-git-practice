e10a03a03dc00a3b8558eefef3b733f9d0616c8b
Shenouda <58535631+ShenoudaWahba@users.noreply.github.com>
2020-03-18 20:12:13 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,8 +1,8 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("brave new world");
-        System.out.println("someone);
-        System.out.println("change one);
+        System.out.println("someone");
+        System.out.println("change one");
         
     }
 }
